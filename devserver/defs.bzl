b41ad5714442d43ce663cbf555da27cc8a43c850
Ryan McDermott <ryantmcdermott@gmail.com>
2022-12-28 21:56:51 -0800

    Checkpoint getting runfiles

--- devserver/defs.bzl
+++ devserver/defs.bzl
@@ -1,10 +1,16 @@
-def devserver(name, port):
+def devserver(name, port, static_file, data = []):
+    normalized_static_file = Label(native.repository_name() + "//" + native.package_name()).relative(static_file)
     native.cc_binary(
         name = name,
         srcs = [
             Label("@rules_devserver//devserver:main"), 
             Label("@rules_devserver//devserver:httplib")
         ],
-        args = ["--port=%d" % port],
+        args = [
+            "--port=%d" % port, 
+            "--static_file=%s" % normalized_static_file
+        ],
+        data = data,
+        deps = ["@bazel_tools//tools/cpp/runfiles"],
         linkopts = ["-lpthread"],
     )
--- devserver/main.cc
+++ devserver/main.cc
@@ -1,8 +1,23 @@
 #include "devserver/httplib/httplib.h"
+#include "tools/cpp/runfiles/runfiles.h"
+
+using bazel::tools::cpp::runfiles::Runfiles;
 
 int main(int argc, char **argv) {
   httplib::Server svr;
   std::string port = "8080";
+
+  std::string runfiles_error;
+  std::unique_ptr<Runfiles> runfiles(
+      Runfiles::Create(argv[0], BAZEL_CURRENT_REPOSITORY, &runfiles_error));
+
+  std::string path = runfiles->Rlocation("devserver/");
+  std::cout << "path: " << path << std::endl;
+
+  // print all args
+  for (int i = 0; i < argc; i++) {
+    std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
+  }
 
   for (int i = 0; i < argc; i++) {
     if (std::string(argv[i]).rfind("--port", 0) != std::string::npos) {
