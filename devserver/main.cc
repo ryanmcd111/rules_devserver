026a5b92c797356ffaf948bf3c69c16ad38c9dd3
Ryan McDermott <ryantmcdermott@gmail.com>
2022-12-28 20:42:41 -0800

    Add clang format

--- devserver/main.cc
+++ devserver/main.cc
@@ -1,11 +1,11 @@
 #include "devserver/httplib/httplib.h"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   httplib::Server svr;
 
-  svr.Get("/", [](const httplib::Request &req, httplib::Response &res)
-          { res.set_content("Hello World!", "text/plain"); });
+  svr.Get("/", [](const httplib::Request &req, httplib::Response &res) {
+    res.set_content("Hello World!", "text/plain");
+  });
 
   svr.listen("localhost", 8080);
 }
