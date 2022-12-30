aa845efef808cbe18f349c7ab7956ac8900c9cb2
Ryan McDermott <ryantmcdermott@gmail.com>
2022-12-29 18:45:30 -0800

    Static file stuff

--- devserver/defs.bzl
+++ devserver/defs.bzl
@@ -1,5 +1,5 @@
 def devserver(name, port, static_file, data = []):
-    normalized_static_file = Label(native.repository_name() + "//" + native.package_name()).relative(static_file)
+    # normalized_static_file = Label(native.repository_name() + "//" + native.package_name()).relative(static_file)
     native.cc_binary(
         name = name,
         srcs = [
@@ -8,7 +8,9 @@ def devserver(name, port, static_file, data = []):
         ],
         args = [
             "--port=%d" % port, 
-            "--static_file=%s" % normalized_static_file
+            "--static_file=%s" % static_file,
+            "--workspace_name=%s" % native.repository_name(),
+            "--package_name=%s" % native.package_name(),
         ],
         data = data,
         deps = ["@bazel_tools//tools/cpp/runfiles"],
--- devserver/main.cc
+++ devserver/main.cc
@@ -1,29 +1,38 @@
+#include <iostream>
+
 #include "devserver/httplib/httplib.h"
 #include "tools/cpp/runfiles/runfiles.h"
 
 using bazel::tools::cpp::runfiles::Runfiles;
 
+constexpr char kWorkspaceName[] = "rules_devserver";
+constexpr char kDefaultPort[] = "8080";
+
 int main(int argc, char **argv) {
   httplib::Server svr;
-  std::string port = "8080";
+  std::string port = std::string(kDefaultPort);
+  std::string static_file;
 
   std::string runfiles_error;
   std::unique_ptr<Runfiles> runfiles(
       Runfiles::Create(argv[0], BAZEL_CURRENT_REPOSITORY, &runfiles_error));
 
-  std::string path = runfiles->Rlocation("devserver/");
+  std::string path = runfiles->Rlocation("rules_devserver/");
   std::cout << "path: " << path << std::endl;
 
-  // print all args
   for (int i = 0; i < argc; i++) {
     std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
-  }
 
-  for (int i = 0; i < argc; i++) {
     if (std::string(argv[i]).rfind("--port", 0) != std::string::npos) {
       port = std::string(argv[i]).substr(7);
     }
+
+    if (std::string(argv[i]).rfind("--static_file", 0) != std::string::npos) {
+      static_file = std::string(argv[i]).substr(14);
+    }
   }
+
+  std::cout << "static_file:" << static_file << std::endl;
 
   svr.Get("/", [](const httplib::Request &req, httplib::Response &res) {
     res.set_content("Hello World!", "text/html");
