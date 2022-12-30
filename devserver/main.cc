01646a5c653f46ea615c4b4e0a11b8f5ea6660a4
Ryan McDermott <ryantmcdermott@gmail.com>
2022-12-29 19:18:35 -0800

    Successfully reading static file

--- devserver/main.cc
+++ devserver/main.cc
@@ -7,22 +7,33 @@ using bazel::tools::cpp::runfiles::Runfiles;
 
 constexpr char kWorkspaceName[] = "rules_devserver";
 constexpr char kDefaultPort[] = "8080";
+constexpr char kHost[] = "localhost";
+
+#define DEBUG true
+#define DEBUG_LOG(msg) \
+  if (DEBUG) std::cout << msg << std::endl;
+
+std::string GetFileContents(const std::string &path) {
+  std::ifstream ifs(path);
+  std::string content((std::istreambuf_iterator<char>(ifs)),
+                      (std::istreambuf_iterator<char>()));
+  return content;
+}
 
 int main(int argc, char **argv) {
   httplib::Server svr;
-  std::string port = std::string(kDefaultPort);
+
+  std::string port = kDefaultPort;
+  std::string workspace_name = kWorkspaceName;
+  std::string package_name;
   std::string static_file;
+  std::string static_file_contents;
 
   std::string runfiles_error;
   std::unique_ptr<Runfiles> runfiles(
       Runfiles::Create(argv[0], BAZEL_CURRENT_REPOSITORY, &runfiles_error));
 
-  std::string path = runfiles->Rlocation("rules_devserver/");
-  std::cout << "path: " << path << std::endl;
-
   for (int i = 0; i < argc; i++) {
-    std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
-
     if (std::string(argv[i]).rfind("--port", 0) != std::string::npos) {
       port = std::string(argv[i]).substr(7);
     }
@@ -30,13 +41,38 @@ int main(int argc, char **argv) {
     if (std::string(argv[i]).rfind("--static_file", 0) != std::string::npos) {
       static_file = std::string(argv[i]).substr(14);
     }
+
+    if (std::string(argv[i]).rfind("--package_name", 0) != std::string::npos) {
+      package_name = std::string(argv[i]).substr(15);
+    }
+
+    if (std::string(argv[i]).rfind("--workspace_name", 0) !=
+        std::string::npos) {
+      const std::string workspace_name_arg = std::string(argv[i]).substr(17);
+      if (workspace_name_arg != "@" && !workspace_name_arg.empty()) {
+        workspace_name = workspace_name_arg;
+      }
+    }
   }
 
+  std::cout << "workspace_name: " << workspace_name << std::endl;
+  std::cout << "package_name: " << package_name << std::endl;
   std::cout << "static_file:" << static_file << std::endl;
 
-  svr.Get("/", [](const httplib::Request &req, httplib::Response &res) {
-    res.set_content("Hello World!", "text/html");
+  std::string path = runfiles->Rlocation(workspace_name + "/");
+  std::cout << "path: " << path << std::endl;
+
+  std::cout << "\nport: " << port << std::endl;
+
+  const std::string static_file_path = path + package_name + "/" + static_file;
+  std::cout << "static_file_path: " << static_file_path << std::endl;
+  static_file_contents = GetFileContents(static_file_path);
+  std::cout << "static_file_contents: " << static_file_contents << std::endl;
+
+  svr.Get("/", [&static_file_contents](const httplib::Request &req,
+                                       httplib::Response &res) {
+    res.set_content(static_file_contents, "text/html");
   });
 
-  svr.listen("localhost", std::stoi(port));
+  svr.listen(kHost, std::stoi(port));
 }
