cf6309dfa9bd69eedc6c014195fe44d9ce03a710
Ryan McDermott <ryantmcdermott@gmail.com>
2022-12-30 09:31:58 -0800

    Add MD5 computation with base64

--- devserver/main.cc
+++ devserver/main.cc
@@ -15,6 +15,9 @@
 using bazel::tools::cpp::runfiles::Runfiles;
 using ::nlohmann::json;
 
+using Path = std::string;
+using FileContents = std::string;
+
 bool DEBUG = false;
 
 #define DEBUG_LOG(msg) \
@@ -23,6 +26,70 @@ bool DEBUG = false;
 constexpr char kWorkspaceName[] = "rules_devserver";
 constexpr int kDefaultPort = 8080;
 constexpr char kHost[] = "localhost";
+
+/*
+ * Base64 encoding/decoding (RFC1341)
+ * Copyright (c) 2005-2011, Jouni Malinen <j@w1.fi>
+ *
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
+ */
+
+// 2016-12-12 - Gaspard Petit : Slightly modified to return a std::string
+// instead of a buffer allocated with malloc.
+
+#include <string>
+
+static const unsigned char base64_table[65] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+/**
+ * base64_encode - Base64 encode
+ * @src: Data to be encoded
+ * @len: Length of the data to be encoded
+ * @out_len: Pointer to output length variable, or %NULL if not used
+ * Returns: Allocated buffer of out_len bytes of encoded data,
+ * or empty string on failure
+ */
+std::string base64_encode(const unsigned char *src, size_t len) {
+  unsigned char *out, *pos;
+  const unsigned char *end, *in;
+
+  size_t olen;
+
+  olen = 4 * ((len + 2) / 3); /* 3-byte blocks to 4-byte */
+
+  if (olen < len) return std::string(); /* integer overflow */
+
+  std::string outStr;
+  outStr.resize(olen);
+  out = (unsigned char *)&outStr[0];
+
+  end = src + len;
+  in = src;
+  pos = out;
+  while (end - in >= 3) {
+    *pos++ = base64_table[in[0] >> 2];
+    *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+    *pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+    *pos++ = base64_table[in[2] & 0x3f];
+    in += 3;
+  }
+
+  if (end - in) {
+    *pos++ = base64_table[in[0] >> 2];
+    if (end - in == 1) {
+      *pos++ = base64_table[(in[0] & 0x03) << 4];
+      *pos++ = '=';
+    } else {
+      *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+      *pos++ = base64_table[(in[1] & 0x0f) << 2];
+    }
+    *pos++ = '=';
+  }
+
+  return outStr;
+}
 
 struct Arguments {
   int32_t port;
@@ -38,10 +105,22 @@ std::string GetFileContents(const std::string &path) {
   return content;
 }
 
-json ComputeManifest() {
+json ComputeManifest(const std::map<Path, FileContents> &path_to_contents) {
   json manifest;
 
-  manifest["manifest"] = {{"/", "123"}};
+  for (const auto &path_and_contents : path_to_contents) {
+    Path path = path_and_contents.first;
+    FileContents contents = path_and_contents.second;
+
+    picohash_ctx_t ctx;
+    char digest[PICOHASH_MD5_DIGEST_LENGTH];
+
+    picohash_init_md5(&ctx);
+    picohash_update(&ctx, &contents, contents.size());
+    picohash_final(&ctx, digest);
+    std::string digest_str(base64_encode(digest, PICOHASH_MD5_DIGEST_LENGTH));
+    manifest[path] = digest_str;
+  }
 
   return manifest;
 }
@@ -93,7 +172,7 @@ Arguments ParseArguments(int argc, char **argv) {
       parser, "static_file", "Index file to serve from top-level / route",
       {"static_file"});
   args::ValueFlag<std::string> workspace_name_arg(
-      parser, "workspace_name", "Calling Bazel workspace name",
+      parser, "workspace_name", "Cgalling Bazel workspace name",
       {"workspace_name"});
   args::ValueFlag<std::string> package_name(parser, "package_name",
                                             "Package name", {"package_name"});
@@ -148,8 +227,10 @@ int main(int argc, char **argv) {
   static_file_contents =
       AddDevserverLoaderToStaticFileContents(static_file_contents);
 
-  const std::map<std::string, std::string> path_to_contents = {{"/", "123"}};
-  const json manifest = ComputeManifest();
+  const std::map<std::string, std::string> path_to_contents = {
+      {"/", static_file_contents}};
+  json manifest = ComputeManifest(path_to_contents);
+  std::cout << "manifest: " << manifest.dump() << std::endl;
 
   svr.Get("/", [&static_file_contents](const httplib::Request &req,
                                        httplib::Response &res) {
@@ -164,8 +245,8 @@ int main(int argc, char **argv) {
       });
 
   svr.Get("/devserver/manifest",
-          [](const httplib::Request &req, httplib::Response &res) {
-            res.set_content(ComputeManifest().dump(), "application/json");
+          [&manifest](const httplib::Request &req, httplib::Response &res) {
+            res.set_content(manifest.dump(), "application/json");
           });
 
   svr.listen(kHost, port);
